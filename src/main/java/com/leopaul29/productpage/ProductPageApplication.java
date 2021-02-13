2f30cdb4feeb1eb0263367191c19f31fb2c4b069
Leo-paul <leopaulmartin@hotmail.fr>
2021-02-13 23:27:34 +0100

    spring initializer

---
+++ src/main/java/com/leopaul29/productpage/ProductPageApplication.java
@@ -0,0 +1,13 @@
+package com.leopaul29.productpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leopaul29/productpage/ProductPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leopaul29.productpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
