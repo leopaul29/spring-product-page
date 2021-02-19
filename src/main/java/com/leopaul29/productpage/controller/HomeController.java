9e6917814dc2e62b38beb9bb241e47a3db962eac
Leo-paul <leopaulmartin@hotmail.fr>
2021-02-19 23:15:27 +0100

    add home and product page

--- src/main/java/com/leopaul29/productpage/controller/HomeController.java
+++ src/main/java/com/leopaul29/productpage/controller/HomeController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -17,9 +18,19 @@ public class HomeController {
     private ProductRepository repository;
 
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String home(Model model) {
         List<Product> productList = repository.findAll();
         model.addAttribute("productList", productList);
         return "home";
+    }
+
+    @GetMapping(value = "/product/{id}")
+    public String productPage(@PathVariable String id, Model model) {
+        Optional<Product> optProduct = repository.findById(id);
+        if(optProduct.isPresent()) {
+            Product product = optProduct.get();
+            model.addAttribute("product", product);
+        }
+        return "product";
     }
 }
